cb75a0ce56bf39dad8ca6dc8a0612fe9d3de5ab6
rensux <renzo.russel@gmail.com>
2022-08-04 13:58:29 +0200

    added ability to separate graphs

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import csv
 import scipy.signal as sig
 import matplotlib.pyplot as plt
 
-N = 2
-Wn = .05 #play with this <0, 1>
+margin = 100  # minimum distance between graphs
+N = 2  # probably don't touch
+Wn = .05  # play with this <0, 1>
 butter = sig.butter(N, Wn, output='sos')
 
 
@@ -22,26 +23,34 @@ def denoise(y: [float]):
     return sig.sosfiltfilt(butter, y)
 
 
-def aggregate(titles: [str], offset=True):
+def aggregate(titles: [str], separate):
+    top = 0
     plt.xlabel("2θ")
     plt.ylabel("Intensity")
     plt.yticks([])
     for title in titles:
         x, y = readxy(title)
-        plt.plot(x, denoise(y))
+        yi = denoise(y)
+        if separate:  # separate the graphs above each other
+            bottom = min(yi)
+            yi += top - bottom + margin
+            top = max(yi)
+        plt.plot(x, yi)
 
-def export(titles: [str], o: str = None):
+
+def export(titles: [str], separate: bool, out: str = None):
     fig = plt.figure()
-    if o is None:
-        o = f'{"+".join(titles)}'
-    o += f'_N={N}_Wn={Wn}'
-    aggregate(titles)
-    plt.savefig(o + '.svg', bbox_inches='tight')
+    if out is None:
+        out = f'{"+".join(titles)}'
+    out += f'_N={N}_Wn={Wn}{"_sep" if separate else ""}'
+    aggregate(titles, separate)
+    plt.savefig(out + '.svg', bbox_inches='tight')
     plt.close(fig)
 
 
-# export(["test1", "test2", "test3"])
+export(["test1", "test2", "test3", "test3", "test3"], True)
+export(["test1", "test2", "test3", "test3", "test3"], False)
 # export(["test2", "test3"], "easdasd")
 
 
-export(["file1", "file2", "file3"])
+# export(["file1", "file2", "file3"])
