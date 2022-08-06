de05b1296c73b4741fc554fdc33822781828a656
rensux <renzo.russel@gmail.com>
2022-08-06 12:46:13 +0200

    added filter and fixed ticks

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def readcsv(path: str, title: str):
 
 def readReflectance(path: str, title: str):
     x, y = readxy(path, title, '.csv', ',', [1, 2])
+    x, y = filterdata(1.5, 6, x, y)
     for i in range(len(x)):
         R = y[i] / 100
         Ri = 1 - R
@@ -67,40 +68,67 @@ def exportgroup(titles: [str], separate: bool, group: str = None, out: str = Non
     plt.close(fig)
 
 
-def exportreflect(title: str):
-    exportcsv(title, readReflectance, True)
+def exportreflect(title: str, flter=None):
+    exportcsv(title, readReflectance, True, flter=flter)
 
 
-def exportcsv(title: str, read=readcsv, minorTicks=False):
+def exportcsv(title: str, read=readcsv, minorTicks=False, flter=None):
     fig = plt.figure()
     out = f'{title}_N={N}_Wn={Wn}'
     x, y = read('data/', title)
+    if flter is not None:
+        x, y = flter(x, y)
     yi = denoise(y)
     line, = plt.plot(x, yi)
     line.set_color('black')
     if minorTicks:
-        ticks, labels = calcTicks(min(x), max(x), 9)
+        ticks, labels = calcTicks(min(x), max(x), 0.1)
         plt.xticks(ticks, labels)
     plt.savefig(f'img/{out}.svg')
     plt.close(fig)
 
 
-def calcTicks(xmin, xmax, steps):
-    ticks = np.arange(math.floor(xmin), xmax + (1 / steps), 1 / steps)
-    labels = np.full(len(ticks), "")
-    for i in range(0, len(ticks), steps):
-        labels[i] = f'{int(round(ticks[i]))}'
-    return ticks, labels
+def calcTicks(xmin: float, xmax: float, minsteps: float, majsteps: int = 1):
+    minticks = []
+    majticks = list(range(math.ceil(xmin), math.ceil(xmax)+majsteps, majsteps))
+
+    for t in np.arange(xmin, xmax + minsteps, minsteps):
+        if abs(round(t) - t) <= minsteps/2:
+            if int(round(t)) % majsteps == 0:
+                continue
+        minticks.append(t)
+
+    majlabels = list(map(lambda x: f'{x}', majticks))
+    minlabels = list(map(lambda x: '', minticks))
+
+    return minticks+majticks, minlabels+majlabels
+
+
+def filterdata(xmin: float, xmax: float, x: [float], y: [float]):
+    xf = []
+    yf = []
+    for i, t in enumerate(x):
+        if xmin <= t <= xmax:
+            xf.append(x[i])
+            yf.append(y[i])
+    return xf, yf
+
+
+def createfilter(xmin: float, xmax: float):
+    return lambda x, y: filterdata(xmin, xmax, x, y)
 
 
 exportgroup(["CAT-3", "CAT-2", "CAT-1", "TiO2-P25", "ZIF-8-MK"], True, "group1")
 
-exportreflect("cat-1-ref")
-exportreflect("cat-2-ref")
-exportreflect("cat-3-ref")
-exportreflect("zif-8-mk-ref")
 
-exportcsv("cat-1")
-exportcsv("cat-2")
-exportcsv("cat-3")
-exportcsv("zif-8-mk")
+reflectfilter = createfilter(1.5, 6)
+exportreflect("cat-1-ref", reflectfilter)
+exportreflect("cat-2-ref", reflectfilter)
+exportreflect("cat-3-ref", reflectfilter)
+exportreflect("zif-8-mk-ref", reflectfilter)
+
+csvfilter = createfilter(200, 1000)
+exportcsv("cat-1", flter=csvfilter)
+exportcsv("cat-2", flter=csvfilter)
+exportcsv("cat-3", flter=csvfilter)
+exportcsv("zif-8-mk", flter=csvfilter)
